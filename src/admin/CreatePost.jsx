34b14f0c68f7ba95c2dc285e8255e4edac5c5074
Ayush Chaurasiya <23je0194@iitism.ac.in>
2025-06-30 13:09:11 +0530

    Another Change

--- src/admin/CreatePost.jsx
+++ src/admin/CreatePost.jsx
@@ -90,7 +90,7 @@ const CreatePost = () => {
             <div className="flex justify-end">
               <button
                 type="submit"
-                className="bg-yellow-500 text-white font-bold px-6 py-3 rounded-md hover:bg-yellow-600 transition duration-300"
+                className="bg-yellow-600 text-white px-6 py-2 rounded-md hover:bg-yellow-700 transition"
               >
                 Publish Post
               </button>
