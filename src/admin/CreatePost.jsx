34228ae2e8636dde384b9c1b6c3b30288fd9d362
Ayush Chaurasiya <23je0194@iitism.ac.in>
2025-06-30 13:04:26 +0530

    Another Change

--- src/admin/CreatePost.jsx
+++ src/admin/CreatePost.jsx
@@ -25,8 +25,14 @@ const CreatePost = () => {
   };
 
   const formats = [
-    "header", "bold", "italic", "underline",
-    "link", "image", "list", "bullet",
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "link",
+    "image",
+    "list",
+    "bullet",
   ];
 
   const handleSubmit = async (e) => {
@@ -82,7 +88,7 @@ const CreatePost = () => {
 
             <button
               type="submit"
-              className="bg-yellow-500 text-white font-semibold px-6 py-2 rounded-md hover:bg-yellow-600 transition duration-300"
+              className="bg-yellow-500 text-white font-bold px-6 py-3 rounded-md hover:bg-yellow-600 transition duration-300 w-full md:w-auto"
             >
               Publish Post
             </button>
